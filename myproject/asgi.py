35279c6bdf878729f90ec3bd42e295fb48d72221
Misba Siddiqui <misbasiddiqui2152@gmail.com>
2026-04-07 11:54:50 +0530

    Update asgi.py

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,9 +1,5 @@
 """
-<<<<<<< HEAD
 ASGI config for myproject project.
-=======
-ASGI config for myproject2 project.
->>>>>>> 20491f934666bd79a5b8c5dc2f756942736540cc
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -15,10 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-<<<<<<< HEAD
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-=======
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject2.settings')
->>>>>>> 20491f934666bd79a5b8c5dc2f756942736540cc
 
 application = get_asgi_application()
